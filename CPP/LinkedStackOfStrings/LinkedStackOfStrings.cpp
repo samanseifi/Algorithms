da6deafc552a8c9c9251a8b075815820363613d0
Saman Seifi <samansei@bu.edu>
2016-11-07 23:18:00 -0500

    stacking algorithm added

--- CPP/LinkedStackOfStrings/LinkedStackOfStrings.cpp
+++ CPP/LinkedStackOfStrings/LinkedStackOfStrings.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class LinkedStackOfStrings {
@@ -22,9 +23,14 @@ public:
 };
 
 int main() {
-	string inputstack = "to be or - not to - - be - that - - is the - -"
-	LinkedStackOfStrings things;
-	
+	LinkedStackOfStrings stack;
+	stack.push("SAMAN");
+	stack.push("SAHAR");
+	cout << stack.pop() << endl;
+	stack.push("NIAZ");
+	stack.push("SOUUDABEH");
+	cout << stack.pop() << endl;
+	cout << stack.pop() << endl;
 	return 0;
 }
 
@@ -35,9 +41,8 @@ bool LinkedStackOfStrings::isEmpty() {
 }
 
 void LinkedStackOfStrings::push(string newitem) {
+	Node* oldfirst = first;
 	first = new Node;	//node should be created!
-	Node* oldfirst;
-	oldfirst = first;
 	first->item = newitem;
 	first->next = oldfirst;
 }
