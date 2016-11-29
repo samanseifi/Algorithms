2bd26e92bde89589131e36ce9ec9b5e4e991f5ba
Saman <samansei@bu.edu>
2016-11-28 19:12:31 -0500

    stiffness matrix for current element

--- CPP/FEM/Main.cpp
+++ CPP/FEM/Main.cpp
@@ -1,5 +1,5 @@
 
-/* 
+/*
 ** This is a simple finite element code for linear elastic material.
 **
 ** 		Young's modulus: E
@@ -9,10 +9,10 @@
 ** Not implemented: stiffness matrix, force matrix, bc, solver
 */
 
-/* Currently makefile is problematic for matrix multiplication with Armadillo! 
+/* Currently makefile is problematic for matrix multiplication with Armadillo!
 ** For time being compile it with:
 **
-** g++ -std=c++11 -larmadillo Main.cpp Mesh.cpp -o fem1
+** g++ Main.cpp Mesh.cpp -o fem1 -std=c++11 -larmadillo
 **
 ** Happy Coding!
 */
@@ -32,22 +32,20 @@ int main(int argc, char *argv[]) {
 
 	// Create a uniform mesh
 	Mesh mesh(4, 2 ,4 ,2);
-	
-	
-	
+
 	// Plane-strain material tangent
 	double E = 100.0;
 	double nu = 0.29;
 
-	mat D(3, 3, fill::zeros);
-	D(0, 0) = D(1, 1) = 1.0 - nu; 
+	Mat<double> D(3, 3, fill::zeros);
+	D(0, 0) = D(1, 1) = 1.0 - nu;
 	D(0, 1) = D(1, 0) = nu;
 	D(2, 2) = 0.5 - nu;
 	D *= E/(1.0 + nu)/(1.0 - 2.0*nu);
 
 	// Constructing the stiffness matrix
-	mat K(2*mesh.num_nodes(), 2*mesh.num_nodes(), fill::zeros);
-	mat q4(4, 2);
+	Mat<double> K(2*mesh.num_nodes(), 2*mesh.num_nodes(), fill::zeros);
+	Mat<double> q4(4, 2);
 	q4(0, 0) = q4(0, 1) = q4(1, 1) = q4(2, 0) = -1.0;
 	q4(1, 0) = q4(2, 1) = q4(3, 0) = q4(3, 1) =  1.0;
 	q4 *= 1.0/sqrt(3.0);
@@ -55,12 +53,13 @@ int main(int argc, char *argv[]) {
 	cout << "Assembling stifness matrix..." << endl;
 
 	// initilizing the B matrix
-	mat B(3, 8, fill::zeros); 
-	mat VV = B*B.t();
-	// loop over elements
+	Mat<double> B(3, 8, fill::zeros);
+	// Mat<double> VV = B*B.t();
+
+	// loop over elements constructing global stiffness matrix K
 	for (int i = 0; i < mesh.num_elements(); i++) {
-		mat xIe(4, 2, fill::zeros);
-		mat curr_elem = mesh.conns_set.row(i);
+		Mat<double> xIe(4, 2, fill::zeros);
+		Mat<double> curr_elem = mesh.conns_set.row(i);
 		int j = 0;
 
 		// loop over current element nodes number (Implementation: C++11)
@@ -68,20 +67,38 @@ int main(int argc, char *argv[]) {
 			xIe.row(j) = mesh.nodes_set.row(val);
 			j++;
 		}
-		// T = trans(xIe);
-		//vec T = (xIe.row(0));
-
-		//cout << xIe << endl;
 
 		// initializing the stiffness matrix of current element
-		mat Ke(8, 8, fill::zeros);
-		mat dN(2, 4, fill::zeros);
-		for (int i = 0; i < q4.n_rows; i++) {
+		Mat<double> Ke(8, 8, fill::zeros);
+		Mat<double> dN(2, 4, fill::zeros);
+		for (int i = 0; i < int(q4.n_rows); i++) {
 			vec q = trans(q4.row(i));  // has to be transposed!!
 			dN = gradshape(q);
+			Mat<double> J = dN * xIe;
+			dN = inv(J) * dN;
 
+			B(0, 0) = dN(0, 0);
+			B(0, 2) = dN(0, 1);
+			B(0, 4) = dN(0, 2);
+			B(0, 6) = dN(0, 3);
+
+			B(1, 1) = dN(1, 0);
+			B(1, 3) = dN(1, 1);
+			B(1, 5) = dN(1, 2);
+			B(1, 7) = dN(1, 3);
+
+			B(2, 0) = dN(1, 0);
+			B(2, 1) = dN(0, 0);
+			B(2, 2) = dN(1, 1);
+			B(2, 3) = dN(0, 1);
+			B(2, 4) = dN(1, 2);
+			B(2, 5) = dN(0, 2);
+			B(2, 6) = dN(1, 3);
+			B(2, 7) = dN(0, 3);
+
+			Ke += det(J) * ((B.t() * D) * B);
 		}
-		cout << dN << "*" << xIe << "=" << dN*xIe << endl;
+		cout << J << endl;
 	}
 	return 0;
 }
@@ -94,24 +111,22 @@ vec shape(vec xi) {
 	double x = xi(0);
 	double y = xi(1);
 
-	// C++11
+	// C++11 implementation
 	vec N = {{(1.0 - x)*(1.0 - y), (1.0+x)*(1.0-y), (1.0+x)*(1.0+y), (1.0-x)*(1.0+y)}};
 	N *= 0.25;
-
 	return N;
 }
 
 /*
-**	Gradient of the shape function for a 4-node, isoparametric element!
+** Gradient of the shape function for a 4-node, isoparametric element!
 */
 mat gradshape(vec xi) {
 	double x = xi(0);
 	double y = xi(1);
 
-	// C++11
-	mat dN = {	{-(1.0 - y),  (1.0 - y), (1.0 + y), -(1.0 + y)},
-				{-(1.0 - x), -(1.0 + x), (1.0 + x),  (1.0 - x)}	}; 
+	// C++11 implementation
+	Mat<double> dN = {{-(1.0 - y),  (1.0 - y), (1.0 + y), -(1.0 + y)},
+				{-(1.0 - x), -(1.0 + x), (1.0 + x),  (1.0 - x)}};
 	dN *= 0.25;
-
 	return dN;
 }
