22c2447f1b5a311abddebcac19a280ced6a08e42
saman <samansei@bu.edu>
2017-01-15 15:55:24 -0500

    fixed the integration

--- CPP/fem1.cpp
+++ CPP/fem1.cpp
@@ -5,97 +5,100 @@
 
 Eigen::MatrixXd FormStiffness(int);
 Eigen::VectorXd FormRHS(int);
-double Integrate(double, double);
+double Integrate_hat1(double, double, double);
+double Integrate_hat2(double, double, double);
 double f(double);
 
-int main() {
 
-	int m = 20;
 
+int main(int argc, char* argv[]) {
+	
+	argc = 1;
+	int m = atoi(argv[1]);	// Number of nodes
+	
 	Eigen::MatrixXd K(m, m);
 	K = FormStiffness(m);
-//	std::cout << K << std::endl;
-
-
+	
 	Eigen::VectorXd b(m);
 	b = FormRHS(m);
-//	std::cout << b << std::endl;
 
 	Eigen::VectorXd x(m);
 	x = K.lu().solve(b);
 	std::cout << x << std::endl;		
 	
-
+	return 0;
 	
 }
 
 Eigen::VectorXd FormRHS(int m) {
 	
-	double h = 1.0/(m + 1);
-	double x[m + 2];
+	double h = 1.0/(m - 1);
+	double  x[m];
 
-	Eigen::VectorXd b(m);
-	
-	for (int i = 0; i < m + 2; i++) {
+	Eigen::VectorXd b(m);	
+
+	for (int i = 0; i < m; i++) {
 		x[i] = i * h;
 	}
 	
-	for (int j = 0; j < m; j++) {
-		if (j == 0) {
-			b(j) = Integrate(0, 1);		
-		} else if (j == m + 1) {
-			b(j) = Integrate(m, m + 1);
-		} else {
-			b(j) = Integrate(j - 1, j) + Integrate(j, j + 1);
-		}
+	b(0) 	 = 0.0; // Account for BC start
+	b(m - 1) = 0.0; // Account for BC end
+
+	for (int j = 1; j < m - 1; j++) {
+		b(j) = Integrate_hat1(x[j - 1], x[j], h) + Integrate_hat2(x[j], x[j + 1], h);
 	}
+	
+	std::cout << b << std::endl;	
 
 	return b;
 }
 
 Eigen::MatrixXd FormStiffness(int m) {
 	
-	double h = 1.0/(m + 1);
+	double h = 1.0/(m - 1);
 
-	Eigen::MatrixXd K(m, m);
+	Eigen::MatrixXd K(m, m );
 	K.setZero(m, m);
 
-	for (int i = 0; i < m; i++)
+	for (int i = 1; i < m - 1; i++)
 		K(i, i) = 2.0;
-	for (int i = 0; i < m - 1; i++) {
+	for (int i = 1; i < m - 2; i++) {
 		K(i, i + 1) = -1.0;
 		K(i + 1, i) = -1.0;
 	}
-
+	
+	K(0, 0) 	= 1.0; // Account for BC start
+	K(m - 1, m - 1) = 1.0; // Account for BC end
+	
+	std::cout << K << std::endl;
 	K *= 1.0/h;
 
 	return K;
 }
 
-double Integrate(double a, double b) {
+double Integrate_hat1(double a, double b, double h) {
+	
+	double c = (a + b)/2.0;                                                                                                    
 
-	double w1 = 1.0;
-	double w2 = 1.0;
+	return ((b - a)/6.0)*(f(a)*((a - a)/h) + 4*f(c)*((c - a)/h) + f(b)*((b - a)/h));	
+}
 
-	double q1 = -1.0/sqrt(3.0);
-	double q2 =  1.0/sqrt(3.0);
+double Integrate_hat2(double a, double b, double h) {
+	
+	double c = (a + b)/2.0;
 
-	double Q1 = q1*(a - b)/2.0 + (a + b)/2.0;
-	double Q2 = q2*(a - b)/2.0 + (a + b)/2.0;
-
-	return ((b - a)/2.0)*(w1*f(Q1)*(Q1 - a)/(b - a) + w2*f(Q2)*(Q2 - a)/(b - a));	
+	return ((b - a)/6.0)*(f(a)*((b - a)/h) + 4*f(c)*((b - c)/h) + f(b)*((b - b)/h));
 }
 
 double f(double x) {
 
-	return sin(x);
+	return 1;
 
 }
 
 
+
 				
-
-
 
 
 
